b9e48c711448e14919529e110f77510c481ddb2a
Kris Scheppe <kris@scheppe.us>
2020-10-28 00:08:49 -0400

    Added column sorting.

--- app/Models/Book.php
+++ app/Models/Book.php
@@ -4,8 +4,16 @@ namespace App\Models;
 
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
+use Kyslik\ColumnSortable\Sortable;
 
 class Book extends Model
 {
     use HasFactory;
+    use Sortable;
+    
+    /**
+     * Field that results can be sorted by.
+     * @var
+     */
+    public $sortable = ['title','author','published'];
 }
